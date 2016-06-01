f49ea511aa1b9adc8808ac0f74a73af6c17ef170
VS <VIVEKLUCKY1848@users.noreply.github.com>
2016-06-01 12:05:09 +0530

    Update useful_snippets.js

--- useful_snippets.js
+++ useful_snippets.js
@@ -1,4 +1,17 @@
-// Check if a variable or object is defined or initialized
+// Check if a variable or object is defined or initialized start
 if (typeof variable !== 'undefined') {
 	// the variable is defined
 }
+
+if (typeof variable === 'undefined') {
+    // variable is undefined or null
+}
+
+if (typeof variable === 'undefined' || variable === null) {
+    // variable is undefined or null
+}
+
+if (typeof variable !== 'undefined' && variable !== null) {
+    // variable is undefined or null
+}
+// Check if a variable or object is defined or initialized finished
